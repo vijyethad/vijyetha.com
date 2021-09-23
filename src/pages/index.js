d4001d3a9ca043b280245ae282e54a3e589d9b2d
Vijyetha Dhanasekaran <d.vijyetha@gmail.com>
2021-09-22 21:49:48 -0500

    feat: Gatsby.js skeleton

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <h1>Vijyetha</h1>
+    </main>
+  )
+}
+
+export default IndexPage
